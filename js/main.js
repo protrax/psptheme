99b9aa6598b92fdebe60a83be088c581013b49d0
protrax <barbariancode@gmail.com>
2014-12-25 18:56:03 +0100

    mase changes  to css for isotope

--- js/main.js
+++ js/main.js
@@ -21,5 +21,53 @@ jQuery(function ($) {
   });
 });
 
+
 })
 
+
+//Isotope functions
+
+(function ($) {
+  var $container = $('.example'),
+    colWidth = function () {
+      var w = $container.width(), 
+        columnNum = 1,
+        columnWidth = 0;
+      if (w > 1200) {
+        columnNum  = 5;
+      } else if (w > 900) {
+        columnNum  = 4;
+      } else if (w > 600) {
+        columnNum  = 3;
+      } else if (w > 300) {
+        columnNum  = 2;
+      }
+      columnWidth = Math.floor(w/columnNum);
+      $container.find('.item').each(function() {
+        var $item = $(this),
+          multiplier_w = $item.attr('class').match(/item-w(\d)/),
+          multiplier_h = $item.attr('class').match(/item-h(\d)/),
+          width = multiplier_w ? columnWidth*multiplier_w[1]-4 : columnWidth-4,
+          height = multiplier_h ? columnWidth*multiplier_h[1]*0.5-4 : columnWidth*0.5-4;
+        $item.css({
+          width: width,
+          height: height
+        });
+      });
+      return columnWidth;
+    },
+    isotope = function () {
+      $container.isotope({
+        resizable: false,
+        itemSelector: '.item',
+        masonry: {
+          columnWidth: colWidth(),
+          gutterWidth: 4
+        }
+      });
+    };
+  isotope();
+  $(window).smartresize(isotope);
+}(jQuery));
+
+//End isotope functions

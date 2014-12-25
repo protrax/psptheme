daf9d9b20bd29a4d553f3575bb0ec78af2402b64
protrax <barbariancode@gmail.com>
2014-12-25 13:17:09 +0100

    Added some bootstrap js to the main js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 jQuery(function ($) {
+  
+  // Enable tooltip
   $('[data-toggle="tooltip"]').tooltip()
+
+  // Fixed sidebar functionality
+  $('#main-sidebar').affix({ 
+  offset: {
+    top: 100,
+    bottom: function () {
+      return (this.bottom = $('.footer').outerHeight(true))
+    }
+  }
 })
+
+
+  //Enable Sidebar offcanvas
+  $(document).ready(function () {
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('.row-offcanvas').toggleClass('active') 
+  });
+});
+
+})
+

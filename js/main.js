dd21f75d46dde721ebd4a0a254dde88a732f1a5a
protrax <barbariancode@gmail.com>
2014-12-27 17:47:24 +0100

    Added animate.css

--- js/main.js
+++ js/main.js
@@ -25,49 +25,100 @@ jQuery(function ($) {
 })
 
 
-//Isotope functions
+//*************************************************
+//****** Start Isotope functions blog masonry *****
+//*************************************************
 
-(function ($) {
-  var $container = $('.example'),
-    colWidth = function () {
-      var w = $container.width(), 
-        columnNum = 1,
-        columnWidth = 0;
-      if (w > 1200) {
-        columnNum  = 5;
-      } else if (w > 900) {
-        columnNum  = 4;
-      } else if (w > 600) {
-        columnNum  = 3;
-      } else if (w > 300) {
-        columnNum  = 2;
+//Isotope function
+jQuery( function($) {
+//Isotope masonry
+$( function() {
+
+    var $container = $('.isotope');
+    $container.isotope({
+      itemSelector: '.item',
+      layoutMode: 'fitRows',
+      masonry: {
+        columnWidth: '.grid-sizer'
       }
-      columnWidth = Math.floor(w/columnNum);
-      $container.find('.item').each(function() {
-        var $item = $(this),
-          multiplier_w = $item.attr('class').match(/item-w(\d)/),
-          multiplier_h = $item.attr('class').match(/item-h(\d)/),
-          width = multiplier_w ? columnWidth*multiplier_w[1]-4 : columnWidth-4,
-          height = multiplier_h ? columnWidth*multiplier_h[1]*0.5-4 : columnWidth*0.5-4;
-        $item.css({
-          width: width,
-          height: height
-        });
-      });
-      return columnWidth;
-    },
-    isotope = function () {
-      $container.isotope({
-        resizable: false,
-        itemSelector: '.item',
-        masonry: {
-          columnWidth: colWidth(),
-          gutterWidth: 4
-        }
-      });
-    };
-  isotope();
-  $(window).smartresize(isotope);
-}(jQuery));
+});
+//Isotope apend on click
+  $('#append').on( 'click', function() {
+    // create new item elements
+    var $elems = getItemElement().add( getItemElement() ).add( getItemElement() );
+    // append elements to container
+    $container.append( $elems )
+      // add and lay out newly appended elements
+      .isotope( 'appended', $elems);
+  });
 
+  // Isotope animation function
+// Dynamic animation class variable
+$animationclass = 'iso-animate ' + 'flipInX';
+// Isotope Animation function
+jQuery(function($) {
+$('.item').each(function(i) {
+  $(this).hide();
+  $(this).show( 100 ).delay((i++) * 100).queue(function( next ){
+        $(this).addClass( $animationclass ); //add a custom class to isotope loading
+        $(this).dequeue();
+        $(this).finish();
+    });
+    });
+  }); // End Isotope animation function
+
+  
+}); // End isotope masonry function
+
+//Load more and generate it
+// make <div class="item width# height#" />
+function getItemElement() {
+  //Append more divs
+  var $item = $('<div class="item">'+
+
+      '<img src="http://placehold.it/300x300" style="max-height:400px; width:100%;"  alt="...">' +
+    '<h2>Article number 1</h2>' + '<p>' + 
+
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' +
+
+
+    '</p>' +
+
+    '<p>' +
+            '<span class="glyphicon glyphicon-calendar">' +
+                'On:December 21, 2014' +
+            '</span>' +
+
+            '<span class="glyphicon glyphicon-heart-empty">' + '120' +
+            '</span>' +
+        '</p>'
+
+
+
+
+
+
+     +'</div>'); 
+
+  // add width and height class
+  var wRand = Math.random();
+  var hRand = Math.random();
+  var widthClass = wRand > 0.85 ? 'width3' : wRand > 0.7 ? 'width2' : '';
+  var heightClass = hRand > 0.85 ? 'height3' : hRand > 0.5 ? 'height2' : '';
+  //Append styles for layout and load animation for the items
+  $item.addClass( widthClass ).addClass( heightClass ).addClass('loading');
+  $('.loading').each(function(i) {
+  $(this).hide();
+  $(this).show( 100 ).delay((i++) * 100).queue(function(  ){
+        $(this).addClass( $animationclass ); //add a custom class to isotope loading
+        $(this).removeClass('loading');
+        $(this).dequeue();
+        $(this).finish();
+    });
+  });
+  return $item;
+}
+}); // End isotope function
 //End isotope functions
+
+

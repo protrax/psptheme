fa99a1283e42450686c2a77ddbc60ea2ab86069d
robba993 <robba_garcia@hotmail.com>
2015-02-05 15:43:02 +0100

    fixed menu, shortcodes, pricing tables, slide, more

--- js/main.js
+++ js/main.js
@@ -656,30 +656,7 @@ ACCORDIONS
 FORM VALIDATION
 *************************************************************/
 
-  $( document ).ready(function() {
-   
-$("#commentForm").validate({
-  rules: {
-    name: "required",
-    email: {
-      required: true,
-      email: true
-    }
-  },
-  messages: {
-    name: "Please specify your name",
-    email: {
-      required: "We need your email address to contact you",
-      email: "Your email address must be in the format of name@domain.com"
-    }
-  }
-});
-$("#commentForm").validate({
-  success: function(label) {
-    label.addClass("valid").text("Ok!")
-  },
-  submitHandler: function() { alert("Submitted!") }
-}); });
+ 
 
 
   /*********** Scroll reveal **********/

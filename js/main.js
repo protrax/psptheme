f77c7bf2ef3d6938578462a35e5c3099571ae6b1
robba993 <robba_garcia@hotmail.com>
2015-02-13 13:49:14 +0100

    google maps fail

--- js/main.js
+++ js/main.js
@@ -754,16 +754,3 @@ Google maps
 *************************************************************/
 
 
-
-      $(function() { 
-       $(window).add(function() {
-          $('#map_canvas').gmap({'center': '57.7973333,12.0502107', 'zoom': 10, 'disableDefaultUI':true, 'callback': function() {
-            var self = this;
-            self.addMarker({'position': this.get('map').getCenter() }).click(function() {
-              self.openInfoWindow({ 'content': 'Hello World!' }, this);
-            }); 
-          }});
-        }).load();
-      });
-
-
